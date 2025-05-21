f14641898c32078bea526407e470238b91866fa4
Isuu1 <jakub.babelek@gmail.com>
2025-05-21 14:14:21 +0100

    Add Header component + install SASS

---
+++ shared/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+//Styles
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Auction Forge</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Template shop</li>
+          <li>Create listing</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
