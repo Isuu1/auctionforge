aa1f1d151ad7756d3c3e4719aa1a1c10c45db6ba
Isuu1 <jakub.babelek@gmail.com>
2025-05-21 17:00:05 +0100

    Add main-layout class + styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
-        {children}
+        <div className="main-layout">
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   );
