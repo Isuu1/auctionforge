7d067987998bdaf2e7a6aa12465b64100ae69724
Isuu1 <jakub.babelek@gmail.com>
2025-05-23 16:10:51 +0100

    Styled CallToActions component

--- features/landing-page/components/CallToActions.tsx
+++ features/landing-page/components/CallToActions.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+//Styles
+import styles from "./CallToActions.module.scss";
 
 const CallToActions = () => {
   return (
-    <div>
-      <h1>Call to actions</h1>
+    <div className={styles.callToActions}>
+      <h1>Craft Stunning eBay Listings with Ease</h1>
+      <p>
+        Discover professional templates and streamline your eBay selling
+        process.
+      </p>
+      <div></div>
     </div>
   );
 };
