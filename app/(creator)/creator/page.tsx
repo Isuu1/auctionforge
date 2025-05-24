2e1bd8502e9441dd4c4ed15080a01dde9db0c6b9
Isuu1 <jakub.babelek@gmail.com>
2025-05-24 18:30:22 +0100

    Add BackLink on creator page

--- app/(creator)/creator/page.tsx
+++ app/(creator)/creator/page.tsx
@@ -1,6 +1,11 @@
+//Icons
+import BackLink from "@/shared/components/BackLink";
+import { FaArrowLeft } from "react-icons/fa";
+
 export default function Page() {
   return (
     <div className="page">
+      <BackLink text="Create new listing" icon={<FaArrowLeft />} />
       <h1>Listing creator</h1>
     </div>
   );
