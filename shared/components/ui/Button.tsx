e6c631a2f90d844564379b2bb86446530b424390
Isuu1 <jakub.babelek@gmail.com>
2025-05-24 17:22:42 +0100

    Replaced scss with Tailwind in Button component

--- shared/components/ui/Button.tsx
+++ shared/components/ui/Button.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-//Styles
-import styles from "./Button.module.scss";
-
 interface ButtonProps {
   text: string;
   icon?: React.ReactNode;
@@ -11,8 +8,25 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ text, icon, variant, size }) => {
+  const baseStyles = "rounded-full border-0 cursor-pointer transition-all";
+
+  const sizeStyles = {
+    small: "px-4 py-2 text-sm",
+    medium: "px-6 py-3 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+
+  const variantStyles = {
+    primary:
+      "bg-yellow-500 text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-blue-500",
+    secondary:
+      "bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-gray-300",
+  };
+
   return (
-    <button className={`${styles.button} ${styles[variant]} ${styles[size]}`}>
+    <button
+      className={`${baseStyles} ${variantStyles[variant]} ${sizeStyles[size]}`}
+    >
       {text}
       {icon && <span>{icon}</span>}
     </button>
