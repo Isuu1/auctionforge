5c4369e36759aef780c83a1c32f052b1fef19dd9
Isuu1 <jakub.babelek@gmail.com>
2025-05-25 11:35:44 +0100

    Add template type + fetch data from backend

--- app/(shop)/shop/page.tsx
+++ app/(shop)/shop/page.tsx
@@ -1,8 +1,14 @@
 import BackLink from "@/shared/components/BackLink";
+import { Template } from "@/shared/types/template";
 //Icons
 import { FaArrowLeft } from "react-icons/fa";
 
-export default function Page() {
+export default async function Page() {
+  const response = await fetch("http://localhost:5245/api/templates");
+  const templates: Template[] = await response.json();
+
+  console.log(templates);
+
   return (
     <div className="page">
       <BackLink text="Browse templates" icon={<FaArrowLeft />} />
---
+++ shared/types/template.ts
@@ -0,0 +1,9 @@
+export type Template = {
+  _id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  content: string;
+  imageUrl?: string;
+};
